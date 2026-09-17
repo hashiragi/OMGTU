aa791034e76fdb5ea7f56314bbb243a97b65ae34
Hashiragi <163924937+hashiragi@users.noreply.github.com>
2026-09-17 16:44:04 +0600

    Add files via upload

---
+++ prac/prac1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во грядок: ");
+            long N = long.Parse(Console.ReadLine());
+            Console.Write("Введите длину грядок: ");
+            long L = long.Parse(Console.ReadLine());
+            Console.Write("Введите ширину грядок: ");
+            long W = long.Parse(Console.ReadLine());
+            Console.Write("Введите расстояние до колодца: ");
+            long K = long.Parse(Console.ReadLine());
+            long S = 2 * N * (L + K) + W * N * (N + 1);
+            Console.WriteLine($"Минимальное расстояние: {S}");
+        }
+    }
+}
